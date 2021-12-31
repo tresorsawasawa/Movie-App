afbabeb163b481f7cf1c375da5c45de13ddbe14a
tresorsawasawa <tresorsawasawa@gmail.com>
2021-12-31 16:44:25 +0200

    Updtate add js/main.js file >> Implement the movie listitems

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
+const moviesList = document.querySelector('.movies-list');
 
 async function getMovies() {
   const response = await fetch(APIURL);
   const responseData = await response.json();
 
+  console.log(responseData);
+
+  responseData.results.forEach((movie) => {
+    const {poster_path, title, vote_average} = movie;
+    moviesList.innerHTML += `<li class="movie mx-2">
+                           <img src="${IMGPATH + poster_path}" alt="${title}">
+                           <div class="movie-infos">
+                             <h3>${title}</h3>
+                             <span>${vote_average}</span>
+                           </div>
+                         </li>
+                        `;
+  });
+
   return responseData;
 }
-
-console.log(getMovies());
+getMovies();
